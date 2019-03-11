076a0f6971d80a25e48c8adbf92f6225e53034b3
WuLiangMeiYue <604675028@qq.com>
2019-03-11 21:05:46 +0800

    修改完善架构

--- src/main/java/com/orchard/api/controller/UserController.java
+++ src/main/java/com/orchard/api/controller/UserController.java
@@ -1,4 +1,14 @@
 package com.orchard.api.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/")
+@Controller
 public class UserController {
+
+    @RequestMapping("/")
+    public String toLogin() {
+        return "login";
+    }
 }
